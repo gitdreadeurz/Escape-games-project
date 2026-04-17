4040edf7f3149ee74e9a4c5e27f057a57122b19d
Nicolas Pollet <you@example.com>
2026-04-17 14:37:10 +0200

    ajout du MVC de la table option

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import optionRoute from './routes/optionRoute.js';
 
 const app = express();
 const port =3000;
 
 app.use(express.json());
+
+app.use('/options', optionRoute);
 
 app.listen(port,()=>{
     console.log(`Serveur express demarre sur http://localhost:${PORT}`);
---
+++ Backend/controllers/optionController.js
@@ -0,0 +1,53 @@
+import { createOptionModel, deleteOptionModel, listOptionModel, optionByIdModel, updateOptionModel } from "../models/OptionModel.js";
+
+//CREATE
+export async function createOptionController(req, res) {
+    try {
+        const libelle = req.body.libelle;
+        const prix = req.body.prix;
+        const result = await createOptionModel(libelle, prix)
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la création de l\'option' });
+    }
+}
+//READ
+export async function listOptionController(req, res) {
+    try {
+        const listOption = await listOptionModel()
+        res.status(200).json(listOption)
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des options' });
+    }
+}
+export async function optionByIdController(req, res) {
+    try {
+        const id = req.params.id;
+        const optionById = await optionByIdModel(id)
+        res.status(200).json(optionById)
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération de l\'option' });
+    }
+}
+//UPDATE
+export async function updateOptionController(req, res) {
+    try {
+        const id = req.params.id;
+        const libelle = req.body.libelle;
+        const prix = req.body.prix;
+        const result = await updateOptionModel(libelle, prix, id)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la mise à jour de l\'option' });
+    }
+}
+//DELETE
+export async function deleteOptionController(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await deleteOptionModel(id)
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la suppression de l\'option' });
+    }
+}
