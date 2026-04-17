ed38096758873aafb9ff8b869ae78e031daaf874
Jérome Surreault <gitdreadeurz@gmail.com>
2026-04-17 16:43:50 +0200

    Version 0.0.0.6 (MVC Avis)

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import optionRoute from './routes/optionRoute.js';
-import userRoutes from './routes/userRoute.js';
+import userRoute from './routes/userRoute.js';
+import avisRoute from './routes/avisRoute.js'
 
 const app = express();
 const port =3000;
 
 app.use(express.json());
 
-app.use('/user', userRoutes);
+app.use('/user', userRoute);
 app.use('/options', optionRoute);
+app.use('/avis', avisRoute);
 
 app.listen(port,()=>{
     console.log(`Serveur express demarre sur http://localhost:${port}`);
