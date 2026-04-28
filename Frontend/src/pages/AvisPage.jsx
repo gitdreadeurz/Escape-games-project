443bcf3f36ab712b4f8d8459b79311af0c385e3f
unknown <yannmonory@gmail.com>
2026-04-28 13:47:42 +0200

    UPDATE AVIS FRONT

--- Frontend/src/pages/AvisPage.jsx
+++ Frontend/src/pages/AvisPage.jsx
@@ -1,13 +1,33 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
+import { useEffect, useState } from "react";
+import { getAllAvis } from '../../service';
 
 function AvisPage() {
-    const avis = [
-        { id: 1, name: "Marie D.", note: 5, text: "Expérience incroyable ! Le manoir hanté nous a fait peur." },
-        { id: 2, name: "Jean P.", note: 4, text: "Très bon moment en équipe. Énigmes bien pensées." },
-        { id: 3, name: "Sophie L.", note: 5, text: "Parfait pour un anniversaire. Je recommande !" }
-    ];
+//     const avis = [
+//         { id: 1, name: "Marie D.", note: 5, text: "Expérience incroyable ! Le manoir hanté nous a fait peur." },
+//         { id: 2, name: "Jean P.", note: 4, text: "Très bon moment en équipe. Énigmes bien pensées." },
+//         { id: 3, name: "Sophie L.", note: 5, text: "Parfait pour un anniversaire. Je recommande !" }
+//     ];
+
+const [avis, setAvis] = useState([]);
+
+const fetchAvis = async () => {
+
+        try {
+            const response = await getAllAvis();
+            console.log(response);
+            setAvis(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(()=>{
+        fetchAvis();
+    },[]);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,10 +40,11 @@ function AvisPage() {
             <div className="page-content">
                 <h1>Avis de nos Clients</h1>
                 
-                <div style={{ marginTop: '2rem' }}>
+                 <div style={{ marginTop: '2rem' }}>
                     {avis.map(avis => (
                         <div key={avis.id} style={{ 
-                            background: 'white', 
+                            background: 'white',
+                            color: '#333', 
                             padding: '1.5rem', 
                             marginBottom: '1rem',
                             borderRadius: '8px',
@@ -33,11 +54,10 @@ function AvisPage() {
                                 <strong>{avis.name}</strong>
                                 <span>{"⭐".repeat(avis.note)}</span>
                             </div>
-                            <p style={{ marginTop: '0.5rem' }}>{avis.text}</p>
+                            <p style={{ marginTop: '0.5rem' }}>{avis.commentaire}</p>
                         </div>
                     ))}
-                </div>
-                
+                </div>                 
                 <h2 style={{ marginTop: '3rem' }}>Laisser un avis</h2>
                 <form onSubmit={handleSubmit} style={{ maxWidth: '600px', marginTop: '1rem' }}>
                     <div className="form-group">
